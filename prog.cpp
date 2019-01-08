3d19914c7aa46079b1a9554b6af2cfd74cafad77
sergmusor <46078083+sergmusor@users.noreply.github.com>
2019-01-08 03:40:06 +0300

    prog.cpp: try to use GTest

--- prog.cpp
+++ prog.cpp
@@ -4,7 +4,11 @@
 
 #include <iostream>
 
-int main() {
+#include <gtest/gtest.h>
+
+int main(int argc, char *argv[]) {
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 	std::cout << "Version: " << ver() << std::endl;
 	std::cout << "Message: " << msg() << std::endl;
 	return EXIT_SUCCESS;
